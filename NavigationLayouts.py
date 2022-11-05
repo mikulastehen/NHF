0f2e140d2a5f70b4c765fc7bac3e46e148edb141
mikulastehen <andraslang2003@gmail.com>
2022-11-05 15:10:10 +0100

    Menu elements separated in library

---
+++ NavigationLayouts.py
@@ -0,0 +1,4 @@
+MainSections = ['Asztalok megadása', 'Menü rögzítése', 'Új asztal nyitása', 'Rendelés felvétele',
+                      'Számla nyomtatása', 'Foglaltsági térkép', 'Kilépés']
+FoodSections = ['Napi menü megjelenítése', 'Napi menü módosítása', 'Napi menü törlése', 'Vissza']
+TableSection = ['Asztalok beállítása', 'Asztalok törlése', 'Vissza']
