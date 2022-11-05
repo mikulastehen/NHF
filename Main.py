4c7f4aeb4a8487c2c191c2ac3c732ac16485bc21
mikulastehen <andraslang2003@gmail.com>
2022-11-05 15:09:59 +0100

    Navigation loops implemented

--- Main.py
+++ Main.py
@@ -1,19 +1,40 @@
-
-import MenuService
-
-
-MainSections = ['Asztalok megadása', 'Menü rögzítése', 'Új asztal nyitása', 'Rendelés felvétele',
-                      'Számla nyomtatása', 'Foglaltsági térkép', 'Kilépés']
-FoodSections = ['Napi menü megjelenítése', 'Napi menü módosítása', 'Napi menü törlése', 'Vissza']
+import Functions
+import NavigationService
+import NavigationLayouts
 
 
-MainMenu = MenuService.Menu(0, MainSections, 'Kajaa')
-FoodMenu = MenuService.Menu(0, FoodSections, 'Napi menü')
+MainNavigation = NavigationService.Navigation(0, NavigationLayouts.MainSections, 'Éttermi Kezelőfelület')
+FoodNavigation = NavigationService.Navigation(0, NavigationLayouts.FoodSections, 'Napi menü')
+TableNavigation = NavigationService.Navigation(0, NavigationLayouts.TableSection, 'Asztal menü')
+Tables = []
+Menu = []
 
 while True:
-    navseq = MainMenu.PrintMenu()
-    if navseq == 1:
-        FoodMenu.PrintMenu()
+    navseq = MainNavigation.PrintMenu()
+    if navseq == 0:
+        while True:
+            navseq = TableNavigation.PrintMenu()
+            if navseq == 0:
+                # Ellenőrizni kell hogy van e visszatérési értéke az asztalnak, mert hiba esetén none-al tér vissza, és problémát okoz a listában!!
+                newtable = Functions.TableAdd(Tables)
+                if newtable is not None:
+                    Tables.append(newtable)
+            elif navseq == 1:
+                Tables = Functions.TableRemove(Tables)
+            elif navseq == 2:
+                break
+
+    elif navseq == 1:
+        while True:
+            navseq = FoodNavigation.PrintMenu()
+            if navseq == 0:
+                pass
+            if navseq == 3:
+                break
+
+    elif navseq == 5:
+        pass
+
     elif navseq == 6:
         exit()
 
