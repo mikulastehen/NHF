eec96d0b27ce0f8805d61e8eef63fcec7f257b28
mikulastehen <andraslang2003@gmail.com>
2022-11-17 22:07:44 +0100

    Added Reservation Layout

--- NavigationLayouts.py
+++ NavigationLayouts.py
@@ -1,4 +1,5 @@
-MainSections = ['Asztalok megadása', 'Menü rögzítése', 'Új asztal nyitása', 'Rendelés felvétele',
+MainSections = ['Asztalbeállítások', 'Napi Menü', 'Asztalfoglalások', 'Rendelés felvétele',
                       'Számla nyomtatása', 'Foglaltsági térkép', 'Kilépés']
-FoodSections = ['Napi menü megjelenítése', 'Napi menü módosítása', 'Napi menü törlése', 'Vissza']
-TableSection = ['Asztalok beállítása', 'Asztalok törlése', 'Vissza']
+FoodSections = ['Menü megjelenítése', 'Menü felvétele', 'Menü törlése', 'Vissza']
+TableSection = ['Asztal felvétele', 'Asztal törlése', 'Vissza']
+ReserveSection = ['Új asztal foglalása', 'Foglalás törlése', 'Vissza']
