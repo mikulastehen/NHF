2f05245ced7b15026235a775457181f913ad2c0d
mikulastehen <andraslang2003@gmail.com>
2022-11-17 22:08:24 +0100

    Added override for equals, and not equals

--- TableData.py
+++ TableData.py
@@ -1,19 +1,22 @@
 class Table:
     def __init__(self, row, column, seats):
-        self.row = row
-        self.column = column
-        self.seats = seats
+        self.row = int(row)
+        self.column = int(column)
+        self.seats = int(seats)
         self.status = 'S'
+        self.order = []
 
     def TableIdent(self):
         return str(self.row)+str(self.column)
 
+    def SIdent(self):
+        return str(self.status)+str(self.seats)
+
     def StatusChange(self, status):
         self.status = status
 
-
-    #Overload-olni kell a komparációt hogy az ülőhelyeket ne vegye figyelembe!
     def __eq__(self, other):
-        if not isinstance(other, Table):
-            raise NotImplementedError
-        return self.row == other.row and self.column == other.column
+        return self.row == other.row and self.column is other.column
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
