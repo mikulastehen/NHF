bf1fbbace73841d7d62d4ebb15167ce6864acbff
mikulastehen <andraslang2003@gmail.com>
2022-11-18 10:56:25 +0100

    made constructor overload

--- TableData.py
+++ TableData.py
@@ -1,10 +1,19 @@
 class Table:
-    def __init__(self, row, column, seats):
-        self.row = int(row)
-        self.column = int(column)
-        self.seats = int(seats)
-        self.status = 'S'
-        self.order = []
+    def __init__(self, *args):
+
+        if len(args) == 4:
+            self.row = int(args[0])
+            self.column = int(args[1])
+            self.status = args[2]
+            self.seats = int(args[3])
+
+        elif len(args) == 3:
+            self.row = int(args[0])
+            self.column = int(args[1])
+            self.seats = int(args[2])
+            self.status = 'S'
+            self.order = []
+
 
     def TableIdent(self):
         return str(self.row)+str(self.column)
