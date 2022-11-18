392e2b1e83b62f426f4d88dc41dbe3a42c82d56d
mikulastehen <andraslang2003@gmail.com>
2022-11-18 10:56:15 +0100

    Added loadfromfile

--- Main.py
+++ Main.py
@@ -3,18 +3,29 @@ import NavigationService
 import NavigationLayouts
 import os
 
+import TableData
+
 MainNavigation = NavigationService.Navigation(0, NavigationLayouts.MainSections, 'Éttermi Kezelőfelület')
 FoodNavigation = NavigationService.Navigation(0, NavigationLayouts.FoodSections, 'Napi menü')
 TableNavigation = NavigationService.Navigation(0, NavigationLayouts.TableSection, 'Asztalok beállítása')
 ReserveNavigation = NavigationService.Navigation(0, NavigationLayouts.ReserveSection, 'Asztalfoglalások')
 Tables = []
 Menu = []
+helyi_konyvtar = os.path.dirname(os.path.realpath(__file__))
+load_data = os.path.join(helyi_konyvtar, 'adatbetoltes.txt')
+tablefile = os.path.join(helyi_konyvtar, 'tables.txt')
+
+with open(tablefile, 'rt') as file:
+    load = file.read().split(',')
+    for x in load:
+        if x is not '':
+            Tables.append(TableData.Table(x[0], x[1], x[2], x[3]))
 
 while True:
 
     #főmenü kiírása
     os.system('mode con: cols=27 lines=20')
-    #navseq = MainNavigation.PrintMenu()
+    navseq = MainNavigation.PrintMenu()
     #navseq = 6
     if navseq == 0:
         #Asztal almenü
